6b28d3e4db40dab45249e793a712eafeff078258
YasinSenpai <90653146+YasinSenpai@users.noreply.github.com>
2022-05-06 21:27:08 +0300

    Update 1.3
    Loli Search için Wikipedia Botu eklendi.

---
+++ BotControl.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Loli Search Bot Control</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <link rel="stylesheet" href="css/mdb.min.css" type="text/css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&family=Quicksand:wght@300&family=Zen+Kaku+Gothic+Antique:wght@300&display=swap" rel="stylesheet">
+    <style>
+        .fontlu{
+            font-family: 'Quicksand', sans-serif;
+        }
+    </style>
+</head>
+<body style="background: #333333">
+
+<div class="container">
+    <div class="mb-5"></div>
+    <div class="row">
+        <form method="post">
+            <div class="card text-white col-md-5" style="background: #3c3c3c">
+                <div class="card-header fontlu text-center"><h5>Wikipedia</h5></div>
+                <div class="card-body">
+                    <div class="alert alert-warning">Lütfen Mobile sitesinden linki yapıştırınız örn: <code>https://tr.<b>m</b>.wikipedia.org/wiki/Vitoria_Muharebesi</code></div>
+                    <input type="url" class="form-control mb-3" name="w_link" placeholder="Wikipedia Mobile Linki yapıştırın" required>
+                    <input type="url" class="form-control" name="w_image" placeholder="Konuyla alakalı Resim" >
+                </div>
+                <div class="card-footer">
+                    <button class="btn btn-outline-success" type="submit" name="wikipedia">Gönder!</button>
+                    <button class="btn btn-outline-danger f-right" type="reset">Reset!</button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <?php
+    require 'LoliBots/Loli_Wiki_Bot.php';
+    ?>
+</div>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.1/moment.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="js/main.js"></script>
+<script src="js/mdb.min.js"></script>
+<script>
+    function realtime() {
+        let time = moment().format('h:mm:ss a');
+        document.getElementById('time').innerHTML = time;
+
+        setInterval(() => {
+            time = moment().format('h:mm:ss a');
+            document.getElementById('time').innerHTML = time;
+        }, 1000)
+    }
+    realtime();
+</script>
+</body>
+</html>
--- sonuc.php
+++ sonuc.php
@@ -126,6 +126,7 @@ session_start();
                 echo '<div class="alert alert-danger">Sayın kullanıcımız +18 sitelere girmek <a href="https://barandogan.av.tr/turk-ceza-kanunu.html#Madde-134">Türk Ceza Kanunun 134-135-136</a> Maddelerini ihlal ediyor olabilir LoliSearch ilerlemeniz durumunda (+18 siteler veya kendine zarar verme başlıklı siteler) hiçbir şekilde zorumluluk kabul etmemektedir.</div>';
             }
             ?>
+            <!--Loli Wiki-->
             <div class="card text-white mb-2" style="background-color: #3c3c3c;">
                 <?php
                 $card_wiki = $db->prepare("SELECT card_baslik, card_muted, card_text, card_image FROM tarayici_card_wiki ");
@@ -156,7 +157,8 @@ session_start();
                 }
                 ?>
             </div>
-        <div class="card text-white mb-2" style="background-color: #3c3c3c;">
+            <!--Loli Anime Wiki-->
+            <div class="card text-white mb-2" style="background-color: #3c3c3c;">
             <?php
             $card_an = $db->prepare("SELECT user_card_title, user_card_muted, user_card_text, user_card_image, user_card_link, user_card_name, user_card_username FROM tarayici_card_anime_users ");
             $card_an->execute(array($q));
@@ -186,6 +188,7 @@ session_start();
             }
             ?>
         </div>
+            <!--Loli Sözlük-->
         <div class="card text-white mb-2 col-md-12"  style="background: #3c3c3c">
             <?php
             $card_sozluk = $db->prepare("SELECT * FROM tarayici_card_sozluk");
@@ -203,6 +206,7 @@ session_start();
             ?>
         </div>
         <div class="col-md-12 text-center">
+            <!--anime_ekle.php zorunlu kılınması-->
             <?php
             require 'php/anime_ekle.php';
             if(isset($_POST['s'])){
@@ -214,11 +218,13 @@ session_start();
             }
             ?>
         </div>
-        <div class="card col-md-7 f-left" style="background-color: #3c3c3c; margin-left: -3px; max-width: 760px;">
+            <!--Web Sonuç-->
+            <div class="card col-md-7 f-left" style="background-color: #3c3c3c; margin-left: -3px; max-width: 760px;">
             <div class="gcse-searchresults-only" runat="server"></div>
             <script async src="https://cse.google.com/cse.js?cx=89ecf14465e74f30b"></script>
         </div>
-        <div class="card text-white f-right col-md-5" style="background-color: #3c3c3c; margin-left: 0px;">
+            <!--Loli Random Anime-->
+            <div class="card text-white f-right col-md-5" style="background-color: #3c3c3c; margin-left: 0px;">
             <?php
             $card_anime = $db->prepare("SELECT tarayici_card_anime_users.user_card_title, tarayici_card_anime_users.user_card_muted, tarayici_card_anime_users.user_card_text, tarayici_card_anime_users.user_card_image, tarayici_card_anime_users.user_card_link, tarayici_card_anime_users.user_card_name, tarayici_card_anime_users.user_card_username tarayici_user_link.user_link_anime, tarayici_user_link.user_link_name, tarayici_user_link.user_link_link FROM tarayici_card_anime_users, tarayici_user_link WHERE tarayici_card_anime_users.user_card_title = tarayici_card_anime_users.user_card_name");
             $anime_sorgu = "SELECT * FROM tarayici_card_anime_users, tarayici_user_link ORDER BY RAND() LIMIT 1";
@@ -264,6 +270,7 @@ session_start();
         </div>
     </div>
     </div>
+    <!--Modallar-->
 
     <div
             class="modal fade"
@@ -587,6 +594,7 @@ session_start();
             role="tabpanel"
             aria-labelledby="pills-profile-tab2"
     >
+        <!--Görsel Arama-->
         <script async src="https://cse.google.com/cse.js?cx=89ecf14465e74f30b"></script>
         <div class="gcse-searchresults-only" id="GoogleImage"></div>
     </div>
