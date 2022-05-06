8d54bc9b6301e874d97e4ad02c4e03c27d6c8e1d
YasinSenpai <90653146+YasinSenpai@users.noreply.github.com>
2022-05-06 18:24:01 +0300

    Update X
    Loli Sözlük eklendi.

--- sonuc.php
+++ sonuc.php
@@ -186,6 +186,22 @@ session_start();
             }
             ?>
         </div>
+        <div class="card text-white mb-2 col-md-12"  style="background: #3c3c3c">
+            <?php
+            $card_sozluk = $db->prepare("SELECT * FROM tarayici_card_sozluk");
+            $card_sozluk->execute(array($q));
+            $card_sozluk_ck = $card_sozluk->fetch(PDO::FETCH_ASSOC);
+            if($card_sozluk_ck > 0){
+                $sozluk_sorgu = "SELECT * FROM tarayici_card_sozluk WHERE sozluk_kelime LIKE '%$q%' LIMIT 1";
+                $sozluk_kontrol = $db->query($sozluk_sorgu);
+                while($cikti = $sozluk_kontrol->fetch(PDO::FETCH_ASSOC)){
+                    echo '<span class="badge badge-info">Loli Sözlük</span><div class="card-header text-center fontlu"><h3>'.$cikti['sozluk_kelime'].'</h3></div>
+                          <div class="card-body fontlu">'.$cikti['sozluk_aciklama'].'</div>';
+
+                }
+            }
+            ?>
+        </div>
         <div class="col-md-12 text-center">
             <?php
             require 'php/anime_ekle.php';
